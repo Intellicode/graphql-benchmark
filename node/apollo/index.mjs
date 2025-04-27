bb123617f6cd48a0315f8e27293c43d5ac0ee7cd
Tom Hastjarjanto <tom@mailx.nl>
2025-04-27 21:53:29 +0200

    feat: add apollo graphql

---
+++ node/apollo/index.mjs
@@ -0,0 +1,15 @@
+import { ApolloServer } from "@apollo/server";
+import { startStandaloneServer } from "@apollo/server/standalone";
+import { loadFiles } from "@graphql-tools/load-files";
+import { resolvers } from "../shared/resolver.mjs";
+
+const server = new ApolloServer({
+  resolvers,
+  typeDefs: await loadFiles("../../schema.graphql"),
+});
+
+const { url } = await startStandaloneServer(server, {
+  listen: { port: 4000 },
+});
+
+console.log(`🚀  Server ready at: ${url}`);
